1e2919698b9c25485d0de144b415b1ecad82ec9f
David-ustc <1753442451@qq.com>
2021-12-30 10:12:34 +0800

    v-f: final version

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from skimage.io.manage_plugins import reset_plugins
 from blending import create_mask, poisson_blend
 import positioning
 import cv2
@@ -6,19 +7,40 @@ from skimage import io
 from scipy.sparse.linalg import spsolve
 #import pyamg
 import matplotlib.pyplot as plt
+mask_path = './input/1mask.jpg'
+src_path='./input/1src.jpg'
+dst_path = './input/1dst.jpg'
+result_path = './output/1.jpg'
 
 if __name__ == "__main__":
-    offset = (56,19)     #(135, 35) # (605,400) ∂‘”¶ (700,550)
-    img_mask = io.imread('./data/mask.jpg', as_gray=True)
-    img_src = io.imread('./data/milanS.jpg').astype(np.float64)
-    img_target = io.imread('./data/milanDst.jpg')
+    img_mask = io.imread(mask_path, as_gray=True)
+    img_src = io.imread(src_path).astype(np.float64)
+    img_target = io.imread(dst_path)
 
-    img_mask, img_src, offset_adj \
+    position = positioning.positioning(dst_path, src_path, mask_path)
+    offset = (position[0], position[1])
+    print("estimated shift of position:", offset)    
+    blend_mask, img_src, offset_adj \
         = create_mask(img_mask.astype(np.float64),
                       img_target, img_src, offset=offset)
 
-    img_pro = poisson_blend(img_mask, img_src, img_target,
+    img_pro = poisson_blend(blend_mask, img_src, img_target,
                             method='normal', offset_adj=offset_adj)
+
+
+
+    def BoundaryPro(img_mask, img_pro):
+        kernel = np.ones((10, 10), np.uint8)
+        img_mask1 = cv2.dilate(img_mask, kernel, iterations = 1)
+        img_mask2 = cv2.erode(img_mask, kernel, iterations = 1)
+
+        cv2.absdiff(img_mask1, img_mask2, img_mask)
+        mask = img_mask.astype(np.uint8)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2RGB)
+        img_pro = cv2.addWeighted(img_pro, 1, mask, 0.1, 0)
+        return img_pro
+
+    img_pro=BoundaryPro(img_mask, img_pro)
     plt.imshow(img_pro)
     plt.show()
-    io.imsave('./data/normalclone.png', img_pro)
+    #io.imsave(result_path, img_pro)
--- positioning.py
+++ positioning.py
@@ -2,12 +2,12 @@ import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 
-def stitching():
-    img1 = cv.imread('./data/milanDst.jpg')
-    img2 = cv.imread('./data/milanS.jpg')
-    img_mask = cv.imread('./data/mask.jpg')
+def positioning(dst_path, src_path, mask):
+    img1 = cv.imread(dst_path)
+    img2 = cv.imread(src_path)
+    img_mask = cv.imread(mask)
     kernel = np.ones((100, 100), np.uint8)
-    img_mask = cv.dilate(img_mask, kernel, iterations = 1)
+    #img_mask = cv.dilate(img_mask, kernel, iterations = 1)
     img_mask = cv.bitwise_not(img_mask)
 
     akaze = cv.AKAZE_create()
@@ -19,10 +19,25 @@ def stitching():
     matches = bf.knnMatch(des1, des2, k=2)
 
     good_matches = []
+    shifts = []
     for m,n in matches:
-        if m.distance < 0.55*n.distance:
-            print(m.queryIdx, m.trainIdx, sep=' ')
-            position =[kp1[m.queryIdx].pt[i]-kp2[m.trainIdx].pt[i] for i in range(2)]
+        if m.distance < 0.75*n.distance:
+            p =[kp1[m.queryIdx].pt[i]-kp2[m.trainIdx].pt[i] for i in range(2)]
+            shifts.append(p)
             good_matches.append([m])
-    position =[int(position[i]/len(good_matches)) for i in range(2)]
-    return position
+
+    img3 = cv.drawMatchesKnn(img1,kp1,img2,kp2,good_matches,None,flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    cv.imwrite('./data/matches.jpg', img3)
+    shifts = abs(np.array(shifts))
+
+    shift =[int(min(shifts[:,1])), int(min(shifts[:,0]))]
+    plt.imshow(img3),plt.colorbar(),plt.show()
+
+    return shift
+
+if __name__ == "__main__":
+    mask_path = './input/3dst.jpg'
+    src_path='./input/3src.jpg'
+    dst_path = './input/3dst.jpg'
+    p = positioning(dst_path, src_path, mask_path)
+    print(p)
